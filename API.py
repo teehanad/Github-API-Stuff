aba5c9918e88b442d98916a4bc7e24912375fb3f
Adam Teehan <teehanad@tcd.ie>
2018-11-13 16:14:33 +0000

    Added in my initial work

---
+++ API.py
@@ -0,0 +1,17 @@
+from github import Github
+
+# First create a Github instance:
+
+# using username and password
+username = input("Enter a username inside quoteation marks: ")
+password = input("Enter your password inside quotation marks: ")
+g = Github(username, password)
+
+# Then play with your Github objects:
+for repo in g.get_user().get_repos():
+    print(repo.name)
+    print(repo.stargazers_count)
+    print("\n")
+    
+username = "null"
+password = "null"
