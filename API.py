33fe781b15530e390e136afc0a643daf958d563e
Adam <teehanad@tcd.ie>
2018-11-28 19:12:26 +0000

    Added in code to get commit dates and commit author for each repo

--- API.py
+++ API.py
@@ -2,8 +2,10 @@ from github import Github
 
 # create a Github instance:
 # using username and password
-username = input("Enter a username inside quoteation marks: ")
-password = input("Enter your password inside quotation marks: ")
+#username = input("Enter a username inside quoteation marks: ")
+#password = input("Enter your password inside quotation marks: ")
+username = "teehanad"
+password = "lonesurvivorAT1"
 g = Github(username, password)
 
 for repo in g.get_user().get_repos():
@@ -13,8 +15,9 @@ for repo in g.get_user().get_repos():
     print("Number of stars: " + str(repo.stargazers_count))
     # Prints the language used in that repo
     print("Language used: " + str(repo.language))
-    # Prints all commits 
-    print("Attributes: " + str(dir(repo)))
+    for commit in repo.get_commits():
+        print commit.commit.author.date
+        print commit.commit.author        
     print("\n")
     
 username = "null"
