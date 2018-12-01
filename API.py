72a6e1ae0348def08a7d2189286a53aa0d42470c
Adam <teehanad@tcd.ie>
2018-12-01 01:58:53 +0000

    eP

--- API.py
+++ API.py
@@ -1,12 +1,21 @@
 from github import Github
 
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
 # create a Github instance:
 # using username and password
-#username = input("Enter a username inside quoteation marks: ")
-#password = input("Enter your password inside quotation marks: ")
-username = "teehanad"
-password = "lonesurvivorAT1"
+username = input("Enter a username inside quoteation marks: ")
+password = input("Enter your password inside quotation marks: ")
 g = Github(username, password)
+
+class Language:
+  def __init__(self, name, count):
+    self.name = name
+    self.count = count
+
+languages = []
 
 for repo in g.get_user().get_repos():
     # Prints names of users repos
@@ -19,6 +28,18 @@ for repo in g.get_user().get_repos():
         print commit.commit.author.date
         print commit.commit.author        
     print("\n")
+
+
+
+y_pos = np.arange(len(objects))
+performance = [10,8,6,4,2,1]
+ 
+plt.bar(y_pos, performance, align='center', alpha=0.5)
+plt.xticks(y_pos, objects)
+plt.ylabel('Usage')
+plt.title('Programming language usage')
+ 
+plt.show()
     
 username = "null"
 password = "null"
